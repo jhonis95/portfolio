8c4811005a914f0a2d96e79c8eb1873010638647
jonatan de oliveira <j.jhonis95@gmail.com>
2021-08-26 08:13:39 +0900

    working in the projects component

--- src/comps/projects.js
+++ src/comps/projects.js
@@ -1,17 +1,33 @@
 import React from 'react'
-import ProjectsView from './projectsView';
+import ProjectsBox from './projectsView';
 class Projects extends React.Component{
-    projects={
-        name:"projeto 1",
-        image:"",
-        description:""
-    };
-    createProjectsView(){
-    }
+    projects=[
+        {
+            name:"Sing In and Sing Up page",
+            image:"",
+            description:"Show my skills in HTML, CSS and JavaScript making a good look and responsive sing in and sing up page",
+            code:"",
+            site:""
+        },
+        {
+            name:"Stream videos page",
+            image:"",
+            description:"Show that i can work in more commercial projects making a stream video page using react and some react libraries to complete the projectow my skills in HTML, CSS and JavaScript making a good look and responsive sing in and sing up page",
+            code:"",
+            site:""
+        }
+    ]
     render(){
         return(
             <div className="projectsContainer">
-                <ProjectsView name={this.projects.name} description="" image=""/>
+                <h2>Projects</h2>
+                <div className="projectsViewContainer">
+                    {
+                        this.projects.map((p)=>(
+                            <ProjectsBox name={p.name} description={p.description} image={p.image} code={p.code} site={p.site}/>
+                        ))
+                    }
+                </div>
             </div>
         );
     }
--- src/comps/projectsView.js
+++ src/comps/projectsView.js
@@ -1,13 +1,16 @@
 import React from 'react'
-class ProjectsView extends React.Component{
+class ProjectsBox extends React.Component{
     render(){
         return(
             <div className="projectViewContainer">
                 <h3>{this.props.name}</h3>
-                <img src="" alt="project img"/>
+                <img src={this.props.image} alt="project img"/>
                 <p>{this.props.description}</p>
+                <p>View:</p> 
+                <button>code</button>{/*using the props code to link the project to github code */}
+                <button>project</button>{/*using the props site to link the project to the site*/}
             </div>
         );
     }
 }
-export default ProjectsView
+export default ProjectsBox
