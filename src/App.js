951b3b67baefd91b17fba93a2b2f6cdce7f2b058
jonatan de oliveira <j.jhonis95@gmail.com>
2021-08-27 08:06:59 +0900

    adding react route to the project

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import './App.css';
 import NavBar from './comps/navBar';
 import Home from './comps/home';
 import Projects from './comps/projects';
+import {BrowserRouter,Switch,Route} from 'react-router-dom'
+/*
+  BrowserRoute to use react route
+  Route to make the route to the links of URL
+  switch to organize what will be show
+*/
 
 function App() {
   return (
     <>
       <NavBar/>
-      <Home/>
-      <Projects/>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/projects" component={Projects}/>
+        </Switch>
+      </BrowserRouter>
     </>
   );
 }
--- src/comps/navBar.js
+++ src/comps/navBar.js
@@ -3,6 +3,7 @@ import './style/navBar.css'
 import NavBtn from './navBtn';
 import Icon from './icon';
 import Logo from './style/images/Logo.png';
+import {Link} from 'react-router-dom'
 class NavBar extends React.Component{
     render(){
     let icon=Logo;
@@ -10,9 +11,15 @@ class NavBar extends React.Component{
             <div className="navbar">
                 <Icon image={icon} text="Jonatan De Oliveira" subtext="front-end developer"/>
                 <nav className="navBtn">
-                    <NavBtn ref="Home" namebtn="home"/>
-                    <NavBtn ref="About" namebtn="about me"/>
-                    <NavBtn ref="Contact" namebtn="contact"/>
+                    {/*<Link to="/">*/}
+                        <NavBtn ref="Home" namebtn="home"/>
+                    {/*</Link>*/}
+                    {/*<Link to="/about">*/}
+                        <NavBtn ref="About" namebtn="about me"/>
+                    {/*</Link>*/}
+                    {/*<Link to="/contact">*/}
+                        <NavBtn ref="Contact" namebtn="contact"/>
+                    {/*</Link>*/}
                 </nav>
             </div>
         );
