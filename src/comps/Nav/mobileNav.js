b3651323e0a2245ec061598dfef3e537f12cca29
jonatan de oliveira <j.jhonis95@gmail.com>
2021-09-15 21:02:16 +0900

    adding a menu in the mobile version

--- src/comps/Nav/mobileNav.js
+++ src/comps/Nav/mobileNav.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import '../style/navBar.css'
 import NavBarLinks from './navBarLinks';
+import {FiMenu} from 'react-icons/fi'
 class MobileNav extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            click: false
+        }
+        this.handerClickMenu=this.handerClickMenu.bind(this);
+    }
+    handerClickMenu(){
+        this.setState(prevState=>({
+            click:!prevState.click
+        }));
+    }
     render(){
         return(
             <div id="mobileNav">
-                <NavBarLinks/>
+                <FiMenu className="hambugerMenu" onClick={this.handerClickMenu}/>
+                {this.state.click&&<NavBarLinks/>}{/*if click is true will show the buttons */}
             </div>
         );
     }
