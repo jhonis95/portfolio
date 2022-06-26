4a98f95f73e79381b386bacbffc8c825dfabb23a
jonatan de oliveira <j.jhonis95@gmail.com>
2022-06-25 22:26:09 -0400

    creating skill component to have all the front end habilities that i have

---
+++ src/comps/skills.js
@@ -0,0 +1,16 @@
+import {Component} from "react";
+
+class skills extends Component{
+    render(){
+        return(
+            <div className="skills-Container">
+                <li>
+                    <h3>skill name</h3>
+                    <img src="" alt=""/> 
+                </li>
+            </div>
+        )
+    }
+}
+
+export default skills;
